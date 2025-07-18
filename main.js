d1e8a6ed49abc835d3a61965003ee0260528daa4
ali.esmaeel@visticle.de <ali.esmaeel@visticle.de>
2025-07-18 17:02:32 +0100

    attribute binding

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
  data : function (){
      return {
-         product : {name:"socks",description:"This is green socks"}
+         product : {name:"socks",description:"This is green socks"},
+         image : "./assets/images/socks_blue.jpg",
+         url : "https://www.google.com",
+         target : "_blank"
      }
  }
 });
